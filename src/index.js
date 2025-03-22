3a6e947cb421d1229fcf1981cd71c066e2575cf9
Mojdeh Khorsand Moghadam <mojdeh.khorsand@gmail.com>
2025-03-22 22:27:59 +0330

    improved css

--- src/index.js
+++ src/index.js
@@ -12,9 +12,11 @@ function generatePoem(event) {
   let apiKey = "663fba8bo73844f66a1c35190804a92t";
   let instructionsInput = document.querySelector("#user-instructions");
   let context =
-    "You are a Romantic Poet AI specializing in crafting unique French poetry. your purpose is to generate a short poem in basic HTML example:<h1>love</h1> seperate each line with </br>. make sure to follow user instructions.";
+    "You are a Romantic Poet AI specializing in crafting unique French poetry. your purpose is to generate a short poem in basic HTML example:<h1>love</h1> seperate each line with </br>. don't type (```html). make sure to follow user instructions.";
   let prompt = `user instructions are: generate a french poem about ${instructionsInput.value}`;
-
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blinking-text"> Thinking... </div>`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(showPoem);
 }
